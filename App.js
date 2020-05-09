1b519a7d72d436841d3c472fe7d213126337e9ff
8989Rahul <daghalerahul0@gmail.com>
2020-05-09 23:25:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hey Rahul</Text>
+    </View>
+  );
+};
+export default App;
