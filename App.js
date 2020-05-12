1e106a75aa0ee7db80f1ad63df3d8ef7ad6c16c0
8989Rahul <daghalerahul0@gmail.com>
2020-05-12 11:59:24 +0530

    using View ,Button,StyleSheet and Text

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,13 +6,40 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('Rahul');
+  const [person, setPerson] = useState({name: 'hey', age: 23});
+
+  const onclickHandler = () => {
+    setName('Rahul Daghale');
+    setPerson({name: 'Rahul', age: 24});
+  };
+
   return (
-    <View>
-      <Text>Hey Rahul</Text>
+    <View style={Styles.container}>
+      <Text>My name is {name} !</Text>
+      <Text>
+        This is {person.name} and the AGE is {person.age}
+      </Text>
+      <View style={Styles.buttonContainer}>
+        <Button title="Update State" onPress={onclickHandler} />
+      </View>
     </View>
   );
 };
 export default App;
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    paddingTop: 40,
+  },
+});
