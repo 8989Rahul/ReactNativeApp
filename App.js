045ad9c39d18cc5a4be6647e58530689e699c98c
8989Rahul <daghalerahul0@gmail.com>
2020-05-14 23:32:04 +0530

    using flatlist

--- App.js
+++ App.js
@@ -5,34 +5,40 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
-  TextInput,
+  FlatList,
+  TouchableOpacity,
 } from 'react-native';
 
 const App = () => {
   const [person, setPerson] = useState([
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
-    {name: 'Rahul'},
+    {name: 'Rahul', id: 1},
+    {name: 'Aahul', id: 2},
+    {name: 'Bahul', id: 3},
+    {name: 'Cahul', id: 4},
+    {name: 'Dahul', id: 5},
+    {name: 'Eahul', id: 6},
+    {name: 'Fahul', id: 7},
+    {name: 'Gahul', id: 8},
+    {name: 'Hahul', id: 9},
   ]);
+
+  const pressHandler = id => {
+    console.log(id);
+    setPerson(prevPerson => prevPerson.filter(person => person.id !== id));
+  };
 
   return (
     <View style={Styles.container}>
-      <ScrollView>
-        {person.map((item, i) => {
-          return (
-            <Text style={Styles.item} key={i}>
-              {item.name}
-            </Text>
-          );
-        })}
-      </ScrollView>
+      <FlatList
+        numColumns={2}
+        data={person}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={Styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
@@ -52,5 +58,6 @@ const Styles = StyleSheet.create({
     padding: 30,
     backgroundColor: 'pink',
     fontSize: 24,
+    marginHorizontal: 20,
   },
 });
