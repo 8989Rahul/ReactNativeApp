be44fdc2a95c37a32089f5f8fba37668e3b19b50
8989Rahul <daghalerahul0@gmail.com>
2020-05-17 23:57:06 +0530

    creating header component

---
+++ Components/Todos/Header.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import {View, FlatList, Text, StyleSheet} from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={Styles.header}>
+      <Text style={Styles.title}>My Todos</Text>
+    </View>
+  );
+};
+export default Header;
+
+const Styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: 'corol',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
