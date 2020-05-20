192d78c883b0998941d7b2d052e210836c1dc36f
8989Rahul <daghalerahul0@gmail.com>
2020-05-20 23:57:51 +0530

    creating TodoItem component

---
+++ Components/Todos/TodoItem.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import {View, FlatList, Text, StyleSheet} from 'react-native';
+
+const TodoItem = () => {
+  return <View />;
+};
+
+export default TodoItem;
